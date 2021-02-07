85fccc30704e04b7da8b86c0a0f61e4510208f9a
Robinborg <robin.borgatrom@gmail.com>
2021-02-07 15:04:54 +0100

    Setting up Pandas dataframe

---
+++ main.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+import matplotlib
+
+data = {
+    'Januari': ['1', '2', '3', '4'],
+    'february': ['1', '2', '3', '4'],
+    'mars': ['1', '2', '3', '4'],
+    'april': ['1', '2', '3', '4'],
+    'may': ['1', '2', '3', '4'],
+    'june': ['1', '2', '3', '4'],
+    'july': ['1', '2', '3', '4'],
+    'august': ['1', '2', '3', '4'],
+    'september': ['1', '2', '3', '4'],
+    'october': ['1', '2', '3', '4'],
+    'november': ['1', '2', '3', '4'],
+    'decemeber': ['1', '2', '3', '4']
+
+}
+
+expense = pd.DataFrame(data)
+income = expense.copy()
+
+  
+
+
